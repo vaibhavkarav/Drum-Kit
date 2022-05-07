fce98d25b632c3eba9641e44d6799c09b94ccc41
Vaibhav Karav <vaibhavkarav99@gmail.com>
2022-05-07 14:21:15 +0530

    implement the drum kit functionalities

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const removeTransition = function (e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+};
+
+const playSound = function (e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+
+  key.classList.add("playing");
+  audio.currentTime = 0; // rewind to the start
+  audio.play();
+};
+
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
